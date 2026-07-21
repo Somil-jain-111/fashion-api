df5c59aad1d13b1a5c407c3940dca71b17e6a68b
Harmeet Singh <harmeet.singh@almonds.ai>
2026-07-21 10:52:28 +0530

    Payment: Cleanup

--- src/modules/payment-verification/payment-verification.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PaymentVerificationService } from './payment-verification.service';
-import { PaymentVerificationController } from './payment-verification.controller';
-
-@Module({
-  controllers: [PaymentVerificationController],
-  providers: [PaymentVerificationService],
-})
-export class PaymentVerificationModule {}
