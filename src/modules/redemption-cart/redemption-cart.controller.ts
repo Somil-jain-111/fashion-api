064214a9de96bb7b58a18ff02f13b54030b175d0
Harmeet Singh <harmeet.singh@almonds.ai>
2026-07-30 11:25:48 +0530

    Cart: Manage/Clear/Get Done

--- src/modules/redemption-cart/redemption-cart.controller.ts
+++ src/modules/redemption-cart/redemption-cart.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Post, Req, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/default/common/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/default/common/guards/roles.guard';
 import { Roles } from 'src/default/common/decorators/roles.decorator';
@@ -11,6 +21,7 @@ import { RedemptionCartService } from './redemption-cart.service';
 import { ManageCartItemDto } from './dto/manage-cart-item.dto';
 import { PlaceCartOrderDto } from '../redemptions/dto/place-cart-order.dto';
 import { VerifyOrderDto } from '../redemptions/dto/verify-order.dto';
+import { RemoveCartItemDto } from '../redemptions/dto';
 
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Roles([UserRole.RETAILER])
@@ -29,6 +40,7 @@ export class RedemptionCartController {
 
   @NoCache()
   @Post()
+  @UseInterceptors(IdempotencyInterceptor)
   @ResponseMessage('Cart updated successfully')
   async manageCartItem(@Req() req: any, @Body() dto: ManageCartItemDto) {
     const userId = Number(req.user.id);
@@ -57,16 +69,18 @@ export class RedemptionCartController {
   }
 
   @NoCache()
-  @Delete('items/:id')
+  @UseInterceptors(IdempotencyInterceptor)
+  @Post('remove-item')
   @ResponseMessage('Item removed from cart successfully')
-  async removeItem(@Req() req: any, @Param('id') id: string) {
+  async removeItem(@Req() req: any, @Body() body: RemoveCartItemDto) {
     const userId = Number(req.user.id);
-    const response = await this.redemptionCartService.removeItem(userId, id);
+    const response = await this.redemptionCartService.removeItem(userId, body.itemId);
     return DataSanitizer.sanitizeData(response);
   }
 
   @NoCache()
-  @Delete('clear')
+  @UseInterceptors(IdempotencyInterceptor)
+  @Post('clear-cart')
   @ResponseMessage('Cart cleared successfully')
   async clearCart(@Req() req: any) {
     const userId = Number(req.user.id);
