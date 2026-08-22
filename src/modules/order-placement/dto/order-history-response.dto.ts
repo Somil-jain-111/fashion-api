e0d32d71397a132907fd63b29b310366679918e2
Harmeet Singh <harmeet.singh@almonds.ai>
2026-08-22 20:07:03 +0530

    OrderPlacement: Summary api

--- src/modules/order-placement/dto/order-history-response.dto.ts
+++ src/modules/order-placement/dto/order-history-response.dto.ts
@@ -1,13 +1,6 @@
 import { OrderPlacementResponseDto } from './order-placement-response.dto';
 
-export interface OrderHistoryMetaDto {
-  page: number;
-  limit: number;
-  totalItems: number;
-  totalPages: number;
-}
-
 export interface OrderHistoryResponseDto {
   items: OrderPlacementResponseDto[];
-  meta: OrderHistoryMetaDto;
+  pagination: Record<string, any>;
 }
